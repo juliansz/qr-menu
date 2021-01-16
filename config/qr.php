a3d08dda77e932fe9f0deb5048486189d446b278
Julian Sznaider <juliansznaider@thenextweb.com>
2021-01-16 18:17:01 -0300

    change config values to be load from env file

--- config/qr.php
+++ config/qr.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'random-slug-size' => 4,
-    'qr-logo-path' => 'img/fast-food.png',
-    'qr-small-size' => 250,
-    'qr-medium-size' => 500,
-    'qr-large-size' => 700,
+    'random-slug-size' => env('RANDOM_SLUG_SIZE', 4),
+    'qr-logo-path' => env('QR_LOGO_PATH', 'img/fast-food.png'),
+    'qr-small-size' => env('QR_SMALL_SIZE', 250),
+    'qr-medium-size' => env('QR_MEDIUM_SIZE', 500),
+    'qr-large-size' => env('QR_LARGE_SIZE', 700),
+    'google-analytics-ua-code' => env('GOOGLE_ANALYTICS_UA_CODE', '')
 ];
