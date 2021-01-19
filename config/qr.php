9043cc8359ab0bd5a57ddff579c94089c5e2fa16
Julian Sznaider <juliansznaider@thenextweb.com>
2021-01-18 22:03:43 -0300

    some comments to document the config file

--- config/qr.php
+++ config/qr.php
@@ -1,10 +1,20 @@
 <?php
 
 return [
+    /* This value is used to determine the size of the random generated slugs */
     'random-slug-size' => env('RANDOM_SLUG_SIZE', 4),
+
+    /* This is the path of the logo to use inside the QR */
     'qr-logo-path' => env('QR_LOGO_PATH', 'img/fast-food.png'),
+
+    /* These are the sizes for the generated QR */
     'qr-small-size' => env('QR_SMALL_SIZE', 250),
     'qr-medium-size' => env('QR_MEDIUM_SIZE', 500),
     'qr-large-size' => env('QR_LARGE_SIZE', 700),
-    'google-analytics-ua-code' => env('GOOGLE_ANALYTICS_UA_CODE', '')
+
+    /* This value, if completed, prints the Google Analytics tracking code in all landing pages */
+    'google-analytics-ua-code' => env('GOOGLE_ANALYTICS_UA_CODE', ''),
+
+    /* This value, if completed, must be the slug of the landing to be shown on the base url */
+    'base-landing-slug' => env('BASE_LANDING_SLUG', ''),
 ];
