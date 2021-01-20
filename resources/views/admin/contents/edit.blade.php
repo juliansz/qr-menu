1f3b3fdf3735422ddc7c03959991b186c24af182
Julian Sznaider <juliansznaider@thenextweb.com>
2021-01-19 22:47:23 -0300

    show file and thumbnail on content edition, disable for now html, link and embedded content types

--- resources/views/admin/contents/edit.blade.php
+++ resources/views/admin/contents/edit.blade.php
@@ -39,9 +39,9 @@
                                 <option value="{{ App\Models\Content::PDF_TYPE }}">{{ App\Models\Content::PDF_TYPE }}</option>
                                 <option value="{{ App\Models\Content::FILE_TYPE }}">{{ App\Models\Content::FILE_TYPE }}</option>
                                 <option value="{{ App\Models\Content::IMAGE_TYPE }}">{{ App\Models\Content::IMAGE_TYPE }}</option>
-                                <option value="{{ App\Models\Content::EMBED_TYPE }}">{{ App\Models\Content::EMBED_TYPE }}</option>
-                                <option value="{{ App\Models\Content::LINK_TYPE }}">{{ App\Models\Content::LINK_TYPE }}</option>
-                                <option value="{{ App\Models\Content::HTML_TYPE }}">{{ App\Models\Content::HTML_TYPE }}</option>
+                                <option value="{{ App\Models\Content::EMBED_TYPE }}" disabled>{{ App\Models\Content::EMBED_TYPE }}</option>
+                                <option value="{{ App\Models\Content::LINK_TYPE }}" disabled>{{ App\Models\Content::LINK_TYPE }}</option>
+                                <option value="{{ App\Models\Content::HTML_TYPE }}" disabled>{{ App\Models\Content::HTML_TYPE }}</option>
                             </select>
                         </div>
                         <div class="form-group">
@@ -56,12 +56,14 @@
                             <label for="file">File input</label>
                             <input type="file" id="file" name="file">
                             <p class="help-block">Upload your file here</p>
+                            @if($content->file_path)<a href="{{ $content->link }}" download >Download file</a>@endif
                         </div>
 
                         <div class="form-group">
                             <label for="file">Thumbnail</label>
                             <input type="file" id="thumbnail" name="thumbnail">
                             <p class="help-block">Upload a thumbnail for preview the content</p>
+                            @if($content->thumbnail_path)<img class="img-thumbnail col-3" src="{{ $content->thumbnail_url }}" />@endif
                         </div>
 
                         <button type="submit" class="btn btn-primary">Submit</button>
